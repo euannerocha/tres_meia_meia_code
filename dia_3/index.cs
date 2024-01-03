d041b51137092e9698d569123dd5a002fb2128b2
Annelise Silva da Rocha <annelisesr@gmail.com>
2024-01-03 18:47:40 -0300

    dia_tres

---
+++ dia_3/index.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        double[] numeros = { 2.5, 3.8, 9.2, 5.1, 7.6 };
+
+        double media = CalcularMedia(numeros);
+        Console.WriteLine($"A média dos números é: {media}");
+    }
+
+
+    static double CalcularMedia(double[] numeros)
+    {
+        if (numeros.Length == 0)
+        {
+            throw new InvalidOperationException("A lista está vazia. Não é possível calcular a média.");
+        }
+
+        double media = numeros.Average();
+        return media;
+    }
+}
