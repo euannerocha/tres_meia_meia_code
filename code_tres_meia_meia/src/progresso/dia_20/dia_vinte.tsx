d043613a1fd7ccf933b5dd1fb06db2098ebb53d8
Annelise Silva da Rocha <annelisesr@gmail.com>
2024-01-20 22:42:30 -0300

    dia_vinte

---
+++ code_tres_meia_meia/src/progresso/dia_20/dia_vinte.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+interface LoginFormState {
+  username: string;
+  password: string;
+}
+
+const LoginForm: React.FC = () => {
+
+  const [formData, setFormData] = useState<LoginFormState>({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent): void => {
+    e.preventDefault();
+    console.log('Formulário enviado:', formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Usuário:
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Senha:
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button type="submit">Entrar</button>
+    </form>
+  );
+};
+
+export default LoginForm;
